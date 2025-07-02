a9a15558b8a50198a5351497ad4ded0827f249c3
Sahdev <Sahdevprajapati27@gmail.com>
2025-07-02 10:21:36 +0000

    variables and 02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+//alert("hello")// we are using nodejs not browser
+//Code readability that should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefind => value is not assigned
+// symbol => unique 
+
+
+//object
+
+console.log(typeof "sahdev"); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
